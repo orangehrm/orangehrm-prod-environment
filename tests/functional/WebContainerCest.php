5df1a97ebe288c8907724588b052a8437b89d8e0
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2017-06-22 17:53:57 +0530

    removing phpmyadmin test case in web functional test

--- tests/functional/WebContainerCest.php
+++ tests/functional/WebContainerCest.php
@@ -19,14 +19,5 @@ class WebContainerCest
     }
 
 
-     public function checkLoginToDBFromPhpmyadmin(AcceptanceTester $I){
-            $I->wantTo("log into mysql 5.5 server through phpmyadmin");
-            $I->amOnPage('http://localhost:9090');
-            $I->fillField('Username:', 'root');
-            $I->fillField('Password:', '1234');
-            $I->click('Go');
-            $I->see('Server: db');
-            $I->see("Server version: 5.5");
-     }
 
 }
