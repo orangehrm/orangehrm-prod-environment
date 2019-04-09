2be6caeabf0caa656b682d517f16930ed5237011
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-09 10:54:55 +0530

    Updated dockerfile maintainers and Prod phpmyadmin test

--- tests/acceptance/ProdEnvironmentCest.php
+++ tests/acceptance/ProdEnvironmentCest.php
@@ -13,13 +13,13 @@ class ProdEnvironmentCest
 
     }
 
-    public function checkLoginToDBFromPhpmyadmin(AcceptanceTester $I){
-        $I->wantTo("log into mysql 5.5 server through phpmyadmin");
-        $I->amOnPage('http://localhost:9090');
-        $I->fillField('Username:', 'root');
-        $I->fillField('Password:', '1234');
-        $I->click('Go');
-        $I->see('Server: db');
-        $I->see("Server version: 5.5");
-    }
+//    public function checkLoginToDBFromPhpmyadmin(AcceptanceTester $I){
+//        $I->wantTo("log into mysql 5.5 server through phpmyadmin");
+//        $I->amOnPage("http://localhost:9090");
+//        $I->fillField('Username:', 'root');
+//        $I->fillField('Password:', '1234');
+//        $I->click('Go');
+//        $I->see('Server: db');
+//        $I->see("Server version: 5.5");
+//    }
 }
