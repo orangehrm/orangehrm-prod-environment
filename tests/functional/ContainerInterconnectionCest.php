3de6d50478226e3cebcde6611e466f3d9a7e9bd3
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-16 15:02:41 +0530

    added functional tests

---
+++ tests/functional/ContainerInterconnectionCest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class ContainerInterconnectionCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+    }
+
+
+    public function connectionBetween_db_and_web(FunctionalTester $I){
+        $I->wantTo("verify php 7.1 ubuntu container is linked with MariaDB 10.2 properly");
+        $I->runShellCommand("docker exec prod_web_ubuntu ping db -c 2");
+        $I->seeInShellOutput('2 packets transmitted, 2 received, 0% packet loss');
+    }
+
+    public function connectionBetween_phpmyadmin_db(FunctionalTester $I){
+        $I->wantTo("verify phpmyadmin container is linked with MariaDB 10.2 container properly");
+        $I->runShellCommand("docker exec prod_phpmyadmin ping db -c 2");
+        $I->seeInShellOutput('2 packets transmitted, 2 packets received, 0% packet loss');
+    }
+
+}
