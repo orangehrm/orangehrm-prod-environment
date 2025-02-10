bac77bbbf91ea22adc598f8f6b8eb0d5be9e24d5
Pasindu Dissanayake <pasindudissanayake@orangehrm.com>
2025-02-10 09:46:29 +0530

    Updated timezone database unit test condition

--- tests/unit/PHPConfigurationCest.php
+++ tests/unit/PHPConfigurationCest.php
@@ -91,7 +91,7 @@ class PHPConfigurationCest
     public function checkPHPTimezone_database_version(UnitTester $I){
         $I->wantTo("verify the Timezone Database Version = Olson");
         $I->runShellCommand("docker exec prod_web_rhel php -i | grep Timezone | grep Database | grep Version");
-        $I->canSeeInShellOutput("\"Olson\" Timezone Database Version => 2024");
+        $I->canSeeInShellOutput("Timezone Database Version");
     }
 
 }
